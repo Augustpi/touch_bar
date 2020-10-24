7a3361e0c7e972e3d736f29112a7368b1287e963
Avni Onur Pehlivan <avnionur@gmail.com>
2020-10-24 18:43:30 +0300

    0.0.5

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const { exec } = require("child_process");
 
 exports.activate = (context) => {
   context.subscriptions.push(
-    vscode.commands.registercommand("ssen.touchbar.run", () => {
+    vscode.commands.registercommand("touchbar.run", () => {
       vscode.commands.executecommand("workbench.action.debug.run");
     }),
-    vscode.commands.registercommand("ssen.touchbar.debug", () => {
+    vscode.commands.registercommand("touchbar.debug", () => {
       vscode.commands.executecommand("workbench.action.debug.start");
     })
   );
 };
 
-exports.deactivate = () => {};
+exports.deactivate = () => { };
